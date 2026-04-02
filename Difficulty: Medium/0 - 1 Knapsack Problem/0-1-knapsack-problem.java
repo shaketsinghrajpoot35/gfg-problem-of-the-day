9357c89f479fd733ef7ecad39a68e4fc6c86abb2
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-04-02 18:24:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,85 +1,119 @@
 
+
+
+
+
+
+//top down approach
+
+
 class Solution {
    
-    private int t[][] ;
-    
-    public int knapsackHalper(int W, int val[], int wt[], int n){
-        
-        
-        if(t[n][W] != -1){
-            return t[n][W];
-        }
-        
-        
-        if(W==0 || n==0){
-            
-            t[0][0] = 0;
-            return 0;//no value add 
-        }
-        
-        
-        int result = 0;
-        
-        if(wt[n-1]<=W){
-            
-            //eight add ot not two option but find max value from both
-            
-            int notAddWt = knapsackHalper(W,val,wt,n-1);
-            int addWt = val[n-1] + knapsackHalper(W-wt[n-1],val,wt,n-1);
-            
-            result= Math.max(addWt,notAddWt);
-            
-           
-        }
-        
-        else if(wt[n-1]>W){
-            
-            
-            result = knapsackHalper(W,val,wt,n-1);
-            
-        }
-        
-        t[n][W] = result;
-        return  t[n][W];
-    }
+  
     
     public int knapsack(int W, int val[], int wt[]) {
         
-        int m = val.length+1;
-        int n = W+1;
         
-        t = new int[m][W+1];
+        //i like n+1 and 
+        //j like W+1
         
-        for(int i=0; i<m;i++){
+        int n = val.length+1;
+        int w = W+1;
+        
+        int t[][] = new int[n][w];
+        
+        for(int i=0; i<n;i++){
             
-            Arrays.fill(t[i],-1);
+            for(int j=0; j<w;j++){
+                
+                if(i==0 || j==0){
+                    t[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<n;i++){
+            
+            for(int j=1; j<w;j++){
+                
+                if(wt[i-1]<=j){
+                    
+                    int take = val[i-1]+t[i-1][j-wt[i-1]];
+                    int notTake = t[i-1][j];
+                    t[i][j] = Math.max(take,notTake);
+                } else{
+                    
+                    t[i][j] = t[i-1][j];
+                }
+            }
         }
       
-      return knapsackHalper(W,val,wt,val.length);
+      return t[n-1][w-1];
         
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// class Solution {
+   
+//     private int t[][] ;
+    
+//     public int knapsackHalper(int W, int val[], int wt[], int n){
+        
+        
+//         if(t[n][W] != -1){
+//             return t[n][W];
+//         }
+        
+        
+//         if(W==0 || n==0){
+            
+//             t[0][0] = 0;
+//             return 0;//no value add 
+//         }
+        
+        
+//         int result = 0;
+        
+//         if(wt[n-1]<=W){
+            
+//             //eight add ot not two option but find max value from both
+            
+//             int notAddWt = knapsackHalper(W,val,wt,n-1);
+//             int addWt = val[n-1] + knapsackHalper(W-wt[n-1],val,wt,n-1);
+            
+//             result= Math.max(addWt,notAddWt);
+            
+           
+//         }
+        
+//         else if(wt[n-1]>W){
+            
+            
+//             result = knapsackHalper(W,val,wt,n-1);
+            
+//         }
+        
+//         t[n][W] = result;
+//         return  t[n][W];
+//     }
+    
+//     public int knapsack(int W, int val[], int wt[]) {
+        
+//         int m = val.length+1;
+//         int n = W+1;
+        
+//         t = new int[m][W+1];
+        
+//         for(int i=0; i<m;i++){
+            
+//             Arrays.fill(t[i],-1);
+//         }
+      
+//       return knapsackHalper(W,val,wt,val.length);
+        
+//     }
+// }
 
 
 
