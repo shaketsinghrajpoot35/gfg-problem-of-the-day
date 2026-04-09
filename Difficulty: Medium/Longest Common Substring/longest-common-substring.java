af4e0037705240c78bc38bbde5151831ca49c8b6
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-04-09 22:46:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,3 +1,49 @@
+// class Solution {
+//     public int longCommSubstr(String s1, String s2) {
+        
+//         int m = s1.length();
+//         int n = s2.length();
+        
+//         int t[][] = new int[m+1][n+1];
+        
+        
+        
+//         for(int i=0;i<m+1;i++){
+            
+//             for(int j=0;j<n+1;j++){
+                
+//                 if(i==0 || j==0){
+//                     t[i][j] = 0;
+//                 }
+//             }
+//         }
+        
+        
+//         int max = 0;
+//         for(int i=1;i<m+1;i++){
+            
+//             for(int j=1;j<n+1;j++){
+                
+//                 if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    
+//                     t[i][j] = 1 + t[i-1][j-1] ;
+                    
+//                     max = Math.max(max, t[i][j]);
+//                 } else{
+                    
+                   
+//                     t[i][j] = 0;
+//                 }
+//             }
+//         }
+        
+     
+//         return max;
+        
+//     }
+
+// }
+
 class Solution {
     public int longCommSubstr(String s1, String s2) {
         
@@ -8,18 +54,17 @@ class Solution {
         
         
         
-        // for(int i=0;i<m+1;i++){
+        for(int i=0;i<m+1;i++){
             
-        //     for(int j=0;j<n+1;j++){
+            for(int j=0;j<n+1;j++){
                 
-        //         if(i==0 || j==0){
-        //             t[i][j] = 0;
-        //         }
-        //     }
-        // }
+                if(i==0 || j==0){
+                    t[i][j] = 0;
+                }
+            }
+        }
         
-        
-        int max = 0;
+     
         for(int i=1;i<m+1;i++){
             
             for(int j=1;j<n+1;j++){
@@ -27,8 +72,7 @@ class Solution {
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
                     
                     t[i][j] = 1 + t[i-1][j-1] ;
-                    
-                    max = Math.max(max, t[i][j]);
+             
                 } else{
                     
                    
@@ -37,8 +81,19 @@ class Solution {
             }
         }
         
-     
+        int max = 0;
+        
+        
+         for(int i=1;i<m+1;i++){
+            
+            for(int j=1;j<n+1;j++){
+                
+                max = Math.max(max, t[i][j]);
+            }
+        }
         return max;
         
     }
+
 }
+    
