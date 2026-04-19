389123bd68910b92dde2cf3afb6b9238d294963c
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-04-19 20:37:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Power/check-for-power.java
@@ -0,0 +1,13 @@
+// Java Code.......
+class Solution {
+    public boolean isPower(int x, int y) {        
+        if(x==1 || x==0)    return false;
+        
+        for(int i=0; i<=1000; i++){
+            int pow = (int)Math.pow(x, i);
+            if(pow==y)  return true;
+            if(pow>y)   return false;
+        }
+        return false;
+    }
+}
